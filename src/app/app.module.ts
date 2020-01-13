b81d22a8029a9beec238ac090bc259b51a18c839
whisgel <whisgeljesus10@gmail.com>
2020-01-13 16:12:46 -0400

    inicio plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ServiciosComponent,
+
   ],
   imports: [
     BrowserModule
